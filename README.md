ef0bfb9db3acec8dbaab085bbf358b6bb109e040
Simon Schumann <simon.schumann@web.de>
2015-12-27 16:10:11 +0100

    initial code upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# gnome-shell-extension-caribou-resize-workspace
+Makes working with Gnome's virtual keyboard "caribou" more pleasant by resizing the workspace on caribou's appearance so that it doesn't cover the window the user want's to type in.
---
+++ src/extension.js
@@ -0,0 +1,19 @@
+
+const Main = imports.ui.main;
+
+function init() {
+
+}
+
+function enable() {
+    // Make caribou resize windows to not cover them
+    Main.layoutManager.removeChrome(Main.layoutManager.keyboardBox);
+    Main.layoutManager.addChrome(   Main.layoutManager.keyboardBox, { affectsStruts: true, trackFullscreen: true });
+
+}
+
+function disable() {
+    // Revert: Make caribou resize windows to not cover them
+    Main.layoutManager.removeChrome(Main.layoutManager.keyboardBox);
+    Main.layoutManager.addChrome(   Main.layoutManager.keyboardBox);
+}
